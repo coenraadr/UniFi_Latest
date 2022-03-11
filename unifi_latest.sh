810e529803d84c7e8e6275b925fabcd88be85eb4
coenraadr <30891751+coenraadr@users.noreply.github.com>
2022-03-11 13:37:50 +0200

    Update unifi_latest.sh

--- unifi_latest.sh
+++ unifi_latest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get install ca-certificates apt-transport-https
-echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+echo "deb https://www.ui.com/downloads/unifi/debian stable ubiquiti" | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
